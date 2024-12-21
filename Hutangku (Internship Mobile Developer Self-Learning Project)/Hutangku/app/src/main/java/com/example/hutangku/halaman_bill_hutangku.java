0abf23ddec3280751fcf3916e1b04e057aa9f599
darrylarkan <117556247+DarrylNa@users.noreply.github.com>
2024-12-21 18:51:49 +0700

    Add files via upload

---
+++ Hutangku (Internship Mobile Developer Self-Learning Project)/Hutangku/app/src/main/java/com/example/hutangku/halaman_bill_hutangku.java
@@ -0,0 +1,14 @@
+package com.example.hutangku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class halaman_bill_hutangku extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_bill_hutangku);
+    }
+}
